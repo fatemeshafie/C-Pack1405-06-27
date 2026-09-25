2c36bd198c33693350949ad44493653a975f337b
fatemeshafie <fatemeshafie77977@gmail.com>
2026-09-25 20:54:30 +0330

    Fibonacci

---
+++ P-4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("How many Fibonacci numbers do you want? ");
+int n = int.Parse(Console.ReadLine());
+
+int first = 0;
+int second = 1;
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(first + " ");
+
+    int next = first + second;
+
+    first = second;
+    second = next;
+}
