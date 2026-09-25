7ddd4554cb475b3108654bc0e5e2976962dfa3db
fatemeshafie <fatemeshafie77977@gmail.com>
2026-09-25 13:11:01 +0330

    update P-1 and add P-2

--- P_1/Program.cs
+++ P_1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+   
+    {
+        Console.Write("what is your name? ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Thanks {name}!");
+    }
